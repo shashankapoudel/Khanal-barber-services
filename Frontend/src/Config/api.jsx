248e41b8b6abd3a9a88b62851884ea6333e3a3a2
Shashanka <poudelshashanka205@gmail.com>
2026-06-17 12:23:35 +0545

    latest changes

--- Frontend/src/Config/api.jsx
+++ Frontend/src/Config/api.jsx
@@ -1,4 +1,4 @@
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "";
+  import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "";
 
 export default BASE_URL;
