b4fc2e7626fab2b1fc9b110f1e588ef6e88d25d7
Shashanka <poudelshashanka205@gmail.com>
2026-06-17 12:52:23 +0545

    changes on api.jsx

--- Frontend/src/Config/api.jsx
+++ Frontend/src/Config/api.jsx
@@ -1,4 +1,6 @@
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8000/api"
+    : "https://sharp-and-mobile-backend.onrender.com/api";
 
 export default BASE_URL;
